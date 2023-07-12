8b92a0c9357ddde087652e64748de44b9f98eed5
Your Parsa <parypary82@gmail.com>
2023-07-12 20:29:53 +0330

    this is my second commit i

---
+++ src/context/products-context.js
@@ -0,0 +1,39 @@
+import { createContext, useState } from "react";
+
+export const ProductsContext = createContext({
+  products: [],
+});
+
+export default function ProductsProvider(props) {
+  const [productsList, setProductsList] = useState([
+    {
+      id: "p1",
+      title: "Red Scarf",
+      description: "A pretty red scarf.",
+      isFavorite: false,
+    },
+    {
+      id: "p2",
+      title: "Blue T-Shirt",
+      description: "A pretty blue t-shirt.",
+      isFavorite: false,
+    },
+    {
+      id: "p3",
+      title: "Green Trousers",
+      description: "A pair of lightly green trousers.",
+      isFavorite: false,
+    },
+    {
+      id: "p4",
+      title: "Orange Hat",
+      description: "Street style! An orange hat.",
+      isFavorite: false,
+    },
+  ]);
+  return (
+    <ProductsContext.Provider value={{ products: productsList }}>
+      {props.children}
+    </ProductsContext.Provider>
+  );
+}
