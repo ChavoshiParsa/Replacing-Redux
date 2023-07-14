7d9779dd3427c53ab838120bc8ec660257ee1753
Your Parsa <parypary82@gmail.com>
2023-07-15 02:12:54 +0330

    buliding our own store just with hooks (not optimized)

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -1,13 +1,11 @@
-import React, { useContext } from "react";
-
 import FavoriteItem from "../components/Favorites/FavoriteItem";
 import "./Products.css";
-import { ProductsContext } from "../context/products-context";
+import { useStore } from "../hooks-store/store";
 
-const Favorites = (props) => {
-  const favoriteProducts = useContext(ProductsContext).products.filter(
-    (p) => p.isFavorite
-  );
+const Favorites = () => {
+  const state = useStore()[0];
+  const favoriteProducts = state.products.filter((p) => p.isFavorite);
+
   let content = <p className="placeholder">Got no favorites yet!</p>;
   if (favoriteProducts.length > 0) {
     content = (
--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,14 +1,12 @@
-import React, { useContext } from "react";
-import { ProductsContext } from "../context/products-context";
-
 import ProductItem from "../components/Products/ProductItem";
 import "./Products.css";
+import { useStore } from "../hooks-store/store";
 
 const Products = (props) => {
-  const productList = useContext(ProductsContext).products;
+  const state = useStore()[0];
   return (
     <ul className="products-list">
-      {productList.map((prod) => (
+      {state.products.map((prod) => (
         <ProductItem
           key={prod.id}
           id={prod.id}
